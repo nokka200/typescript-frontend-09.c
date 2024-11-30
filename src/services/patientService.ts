ce74a47217aa62d79172fa8d09cda53116e5ac59
Niko Niemelä <nokka200@gmail.com>
2024-11-30 10:49:26 +0200

    Aloitettu 9.11.

--- src/services/patientService.ts
+++ src/services/patientService.ts
@@ -2,7 +2,13 @@ import { NonSensitivePatient } from "../types";
 import patientEntries from "../../data/patient";
 
 const getNonSensitivePatients = (): NonSensitivePatient[] => {
-  return patientEntries;
+  return patientEntries.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
 };
 
 
