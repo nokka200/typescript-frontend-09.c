b73d87d599f9b8f2189bde7a3a3df19b11146eb4
Niko Niemelä <nokka200@gmail.com>
2024-12-02 10:55:11 +0200

    Tehtävä 9.11. valmis ja frontend toimii

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import patientRouter from './routes/patients';
 import diagnossesService from './routes/diagnosess';
+import cors from 'cors';
 const app = express();
+const PORT = 3001;
+const FRONT_END_PORT = 5173;
+
+app.use(cors(
+  {
+    origin: `http://localhost:${FRONT_END_PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }
+));
 app.use(express.json());
 
-const PORT = 3000;
+
 
 app.use('/api/patients', patientRouter);
 app.use('/api/diagnoses', diagnossesService);
